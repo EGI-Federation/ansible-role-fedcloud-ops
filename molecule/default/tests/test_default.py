1ba49ed19a73698ad220b9f392a673c3bf9e7344
Enol Fernández <enol.fernandez@egi.eu>
2021-06-18 08:32:47 +0100

    Add regions (#15)
    * Add regions here
    
    * Add test for the region
    
    * Fix linting issues
    
    * Do not fail if value not defined
    
    * Reorder keys to make linter happy
    
    * Disable env linting
    
    * Ansible linting
    
    * More linting

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -20,5 +20,18 @@ def test_site_files(host):
     filename = "foo-bar-%s" % endpoint_hash
     assert host.file("/etc/egi/cloud-info/").is_directory
     assert host.file("/etc/egi/cloud-info/%s.yaml" % filename).exists
+    assert not host.file("/etc/egi/cloud-info/%s.env" % filename).contains("OS_REGION")
     assert host.file("/etc/egi/cloud-info/%s.env" % filename).exists
+    assert host.file("/etc/cron.d/cloud-info-%s" % filename).exists
+
+
+def test_site_files_region(host):
+    endpoint_hash = hashlib.md5(b"https://site.org:5000/v3/").hexdigest()
+    filename = "bar-foo-%s" % endpoint_hash
+    assert host.file("/etc/egi/cloud-info/").is_directory
+    assert host.file("/etc/egi/cloud-info/%s.yaml" % filename).exists
+    assert host.file("/etc/egi/cloud-info/%s.env" % filename).exists
+    assert host.file("/etc/egi/cloud-info/%s.env" % filename).contains(
+        "OS_REGION=region1"
+    )
     assert host.file("/etc/cron.d/cloud-info-%s" % filename).exists
